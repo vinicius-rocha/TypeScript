9b4a254b67c9d68d1104c11aa56dd2f180e83fd7
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-01 23:14:06 -0300

    Modelando com TypeScript

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,2 +1,3 @@
 let negociacao = new Negociacao(new Date(), 2, 100);
+negociacao._quantidade = 3; /* O VSCODE indica um erro de compilação aqui*/ /*[ts] Property '_quantidade' is private and only accessible within class 'Negociacao'.*/
 console.log(negociacao);
