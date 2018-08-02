1a6d1913fba580195897d8fb99dba982ce4b5ada
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-01 23:17:39 -0300

    Automatizando o processo de compilação

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,3 +1,2 @@
 let negociacao = new Negociacao(new Date(), 2, 100);
-negociacao._quantidade = 3; /* O VSCODE indica um erro de compilação aqui*/ /*[ts] Property '_quantidade' is private and only accessible within class 'Negociacao'.*/
 console.log(negociacao);
