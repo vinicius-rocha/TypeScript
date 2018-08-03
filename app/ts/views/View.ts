253775d10a1278764a5bdf4388cd2aae6c113be7
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-02 23:15:03 -0300

    Classes abstradas e métodos abstrados
    - Recursos novos disponíveis no TypeScript.

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,4 +1,4 @@
-class View<T> {
+abstract class View<T> {
 
     private _elemento: Element;
 
@@ -10,7 +10,5 @@ class View<T> {
         this._elemento.innerHTML = this.template(model);
     }
 
-    template(model: T): string {
-        throw new Error('Você deve implementar o método template.')
-    }
+    abstract template(model: T): string;
 }
