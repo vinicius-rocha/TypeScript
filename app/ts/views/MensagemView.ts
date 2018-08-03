426620b30223edee5389ea4af1d6c0b51c7420df
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-02 23:05:34 -0300

    Lidando com tipos genéricos

--- app/ts/views/MensagemView.ts
+++ app/ts/views/MensagemView.ts
@@ -1,8 +1,4 @@
-class MensagemView extends View{
-
-    update(model: string) {
-        this._elemento.innerHTML = this.template(model);
-    }
+class MensagemView extends View<string>{
 
     template(model: string): string {
         return `<p class="alert alert-info">${model}</p>`;
--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,8 +1,16 @@
-class View {
+class View<T> {
 
-    protected _elemento: Element;
+    private _elemento: Element;
 
     constructor(seletor: string) {
         this._elemento = document.querySelector(seletor);
+    }
+
+    update(model: T) {
+        this._elemento.innerHTML = this.template(model);
+    }
+
+    template(model: T): string {
+        throw new Error('Você deve implementar o método template.')
     }
 }
