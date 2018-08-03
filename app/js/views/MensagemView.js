a897d721f0c6627be0c0f00e13f49a27cbcb50f3
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-02 22:51:23 -0300

    Herança e a vantagem 'protected'

--- app/js/views/MensagemView.js
+++ app/js/views/MensagemView.js
@@ -1,7 +1,4 @@
-class MensagemView {
-    constructor(seletor) {
-        this._elemento = document.querySelector(seletor);
-    }
+class MensagemView extends View {
     update(model) {
         this._elemento.innerHTML = this.template(model);
     }
--- app/ts/views/MensagemView.ts
+++ app/ts/views/MensagemView.ts
@@ -1,10 +1,4 @@
-class MensagemView {
-
-    private _elemento: Element;
-
-    constructor(seletor: string) {
-        this._elemento = document.querySelector(seletor);
-    }
+class MensagemView extends View{
 
     update(model: string) {
         this._elemento.innerHTML = this.template(model);
