c0634f2bb9dffe3ccea27b1537ca71b59ed92c0f
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-03 20:37:59 -0300

    Parâmetros opcionais

--- app/js/views/View.js
+++ app/js/views/View.js
@@ -6,11 +6,14 @@ System.register([], function (exports_1, context_1) {
         setters: [],
         execute: function () {
             View = class View {
-                constructor(seletor) {
+                constructor(seletor, escapar) {
                     this._elemento = $(seletor);
                 }
                 update(model) {
-                    this._elemento.html(this.template(model));
+                    let template = this.template(model);
+                    if (this._escapar)
+                        template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+                    this._elemento.html(template);
                 }
             };
             exports_1("View", View);
--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,13 +1,17 @@
 export abstract class View<T> {
 
     private _elemento: JQuery;
+    private _escapar: boolean;
 
-    constructor(seletor: string) {
+    constructor(seletor: string, escapar?: boolean) {
         this._elemento = $(seletor);
     }
 
     update(model: T) {
-        this._elemento.html(this.template(model));
+        let template = this.template(model);
+        if(this._escapar) 
+            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+        this._elemento.html(template);
     }
 
     abstract template(model: T): string;
