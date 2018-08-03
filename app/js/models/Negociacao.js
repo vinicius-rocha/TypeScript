91398729cb9190393b579dd980d61b8b18d5fdb8
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-03 14:05:17 -0300

    Carregador de Módulos - System.js
    - Utilizando o System.js, para carregar os módulos de nossa aplicação.
    Configurando o tsconfig.json para que o código compilado seja
    compatível.

--- app/js/models/Negociacao.js
+++ app/js/models/Negociacao.js
@@ -1,19 +1,30 @@
-export class Negociacao {
-    constructor(data, quantidade, valor) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
-    get data() {
-        return this._data;
-    }
-    get quantidade() {
-        return this._quantidade;
-    }
-    get valor() {
-        return this._valor;
-    }
-    get volume() {
-        return this._quantidade * this._valor;
-    }
-}
+System.register([], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var Negociacao;
+    return {
+        setters: [],
+        execute: function () {
+            Negociacao = class Negociacao {
+                constructor(data, quantidade, valor) {
+                    this._data = data;
+                    this._quantidade = quantidade;
+                    this._valor = valor;
+                }
+                get data() {
+                    return this._data;
+                }
+                get quantidade() {
+                    return this._quantidade;
+                }
+                get valor() {
+                    return this._valor;
+                }
+                get volume() {
+                    return this._quantidade * this._valor;
+                }
+            };
+            exports_1("Negociacao", Negociacao);
+        }
+    };
+});
