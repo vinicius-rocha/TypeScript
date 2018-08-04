d33d07e6fadf87137ec18b1d0b9d0770499f4ff1
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-04 12:16:43 -0300

    Padrão de Projeto Decorator
    - Criando o esqueleto para Decorator de método.

---
+++ app/ts/helpers/decorators/logarTempoDeExecucao.ts
@@ -0,0 +1,12 @@
+export function logarTempoDeExecucao() {
+    
+    return function(target: any, propertyKey: string, descriptor: PropertyDescriptor){
+
+        const metodoOriginal = descriptor.value;
+
+        descriptor.value = function(...args: any[]){
+            const retorno = metodoOriginal.apply(this, args);
+            return retorno;
+        }
+    }
+}
