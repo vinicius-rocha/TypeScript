040bcf7e0ee5181da8e5b00362f073f4a1001468
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-04 13:49:59 -0300

    Decorator para DOM Inject
    - usando o decorator para injetar nossas propriedades com o padrão Lazy
    Loading.

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,21 +1,20 @@
 import { NegociacoesView, MensagemView } from '../views/index';
 import { Negociacoes, Negociacao } from '../models/index';
+import { domInject } from '../helpers/decorators/index';
 
 export class NegociacaoController {
 
+    @domInject('#data')
     private _inputData: JQuery;
+    @domInject('#quantidade')
     private _inputQuantidade: JQuery;
+    @domInject('#valor')
     private _inputValor: JQuery;
     private _negociacoes = new Negociacoes();
     private _negociacoesView = new NegociacoesView('#negociacoesView',true);
     private _mensagemView = new MensagemView('#mensagemView');
 
     constructor() {
-
-        this._inputData = $('#data');
-        this._inputQuantidade = $('#quantidade');
-        this._inputValor = $('#valor');
-
         this._negociacoesView.update(this._negociacoes);
     }
 
