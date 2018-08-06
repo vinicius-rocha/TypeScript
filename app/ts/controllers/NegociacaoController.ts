cfcef9a1776c88c693086e8a5b03351c49582f1c
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-06 11:08:40 -0300

    Definindo uma interface para a API
    - Utilizando interface para definir os campos consumidos de nossa API.

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,5 +1,5 @@
 import { NegociacoesView, MensagemView } from '../views/index';
-import { Negociacoes, Negociacao } from '../models/index';
+import { Negociacoes, Negociacao, NegociacaoParcial } from '../models/index';
 import { domInject } from '../helpers/decorators/index';
 
 export class NegociacaoController {
@@ -51,7 +51,7 @@ export class NegociacaoController {
         fetch('http://localhost:8080/dados')
             .then(res => isOk(res))
             .then(res => res.json())
-            .then((dados: any[]) => {
+            .then((dados: NegociacaoParcial[]) => {
                 dados
                     .map(dado => new Negociacao(new Date(), dado.vezes, dado.montante))
                     .forEach(negociacao => this._negociacoes.adiciona(negociacao));
