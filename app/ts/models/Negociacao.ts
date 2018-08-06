44433a7442ea808096d8c365ecaa1e4b91c78f87
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-06 16:31:23 -0300

    Polimorfismo com Interface
    - Substituindo o uso da classe abstrata com métodos abtrados por uso de
    Interface.
    - No TypeScript a classe pode herdar apenas de uma classe, e
    considerando que apenas queríamos passar a necessidade de implementação
    de um método, faz mais sentido o uso de interface.

--- app/ts/models/Negociacao.ts
+++ app/ts/models/Negociacao.ts
@@ -1,15 +1,13 @@
 import { Imprimivel } from "./Imprimivel";
 
-export class Negociacao extends Imprimivel{
-    
-    constructor(readonly data: Date, readonly quantidade: number, readonly valor: number) {
-        super();
-    }
-    
+export class Negociacao implements Imprimivel {
+
+    constructor(readonly data: Date, readonly quantidade: number, readonly valor: number) { }
+
     get volume() {
         return this.quantidade * this.valor;
     }
-    
+
     paraTexto(): void {
         console.log('-- paraTexto --');
         console.log(
